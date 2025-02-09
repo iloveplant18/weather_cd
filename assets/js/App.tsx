e36288c5017a05b0200318ca9b0b7d641a474f09
iloveplant18 <degradant31@gmail.com>
2025-02-09 22:49:24 +0300

    add not found page

--- assets/js/App.tsx
+++ assets/js/App.tsx
@@ -5,17 +5,19 @@ import Favorites from "./Pages/Favorites/Favorites.tsx";
 import Settings from "./Pages/Settings/Settings.tsx";
 import Location from "./Pages/Location/Location.tsx";
 import HoursForecast from "./Pages/HoursForecast/HoursForecast.tsx";
+import NotFound from "./Pages/NotFound/NotFound.tsx";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="" element={<DefaultLayout />}>
-          <Route index element={<Home />} />
+          <Route path="" element={<Home />} />
           <Route path="favorites" element={<Favorites />} />
           <Route path="settings" element={<Settings />} />
           <Route path="location/:locationId" element={<Location />} />
           <Route path="location/:locationId/:date" element={<HoursForecast />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </HashRouter>
---
+++ assets/js/Pages/NotFound/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div className="pt-10">
+      <h1 className="text-3xl text-center font-bold">Page not found ¯\_(ツ)_/¯</h1>
+    </div>
+  );
+}
+
+export default NotFound;
